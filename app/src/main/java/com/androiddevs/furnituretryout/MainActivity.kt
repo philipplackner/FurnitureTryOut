360b48a084dce2866156685b6503eaf258607c1d
Philipp <philipp.lackner@yahoo.de>
2020-03-31 13:18:24 +0200

    Part 2.2 - BottomSheet is fully working

--- app/src/main/java/com/androiddevs/furnituretryout/MainActivity.kt
+++ app/src/main/java/com/androiddevs/furnituretryout/MainActivity.kt
@@ -7,11 +7,32 @@ import android.view.View
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
 
+private const val BOTTOM_SHEET_PEEK_HEIGHT = 50f
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupBottomSheet()
+    }
+
+    private fun setupBottomSheet() {
+        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.peekHeight =
+            TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                BOTTOM_SHEET_PEEK_HEIGHT,
+                resources.displayMetrics
+            ).toInt()
+        bottomSheetBehavior.addBottomSheetCallback(object :
+        BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                bottomSheet.bringToFront()
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {}
+        })
     }
 
 }
