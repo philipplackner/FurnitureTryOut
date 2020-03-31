0d29533e74c780879cd0a359be4a9dd05d2fd4b5
Philipp <philipp.lackner@yahoo.de>
2020-03-31 12:43:28 +0200

    Initial commit

---
+++ app/src/main/java/com/androiddevs/furnituretryout/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.androiddevs.furnituretryout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
