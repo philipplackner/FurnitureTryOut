cf914cce2fdd2dfad5cd01ca556441a46274ef61
Philipp <philipp.lackner@yahoo.de>
2020-04-01 16:41:59 +0200

    Part 2.12 - WRITE_EXTERNAL_STORAGE permission added

---
+++ app/src/main/java/com/androiddevs/furnituretryout/CustomArFragment.kt
@@ -0,0 +1,17 @@
+package com.androiddevs.furnituretryout
+
+import android.Manifest
+import com.google.ar.sceneform.ux.ArFragment
+
+class CustomArFragment : ArFragment() {
+
+    override fun getAdditionalPermissions(): Array<String> {
+        val additionalPermissions = super.getAdditionalPermissions()
+        val permissionsLength = additionalPermissions.size
+        val permissions = Array(permissionsLength + 1) { Manifest.permission.WRITE_EXTERNAL_STORAGE }
+        if(permissionsLength > 0) {
+            System.arraycopy(additionalPermissions, 0, permissions, 1, permissionsLength)
+        }
+        return permissions
+    }
+}
